65c59566030d85013d58a1fb0ffa3c8f78cb038f
natanista <gdaynatan@gmail.com>
2020-09-11 12:39:45 -0300

    script mySQL adicionado

--- Projeto_Alpha/Banco de Dados/My_SQL_script.sql
+++
@@ -1,1 +0,0 @@
-create database projeto_pi;
---
+++ Projeto_Alpha/Banco de Dados/Natan/script-mySQL.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE empresa;
+
+CREATE TABLE login(
+	id_login INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	login varchar(40) NOT NULL,
+    senha varchar(40) NOT NULL
+ );
+ 
+ CREATE TABLE informacoes(
+	id_info INT  NOT NULL,
+    first_name varchar(40),
+    last_name varchar(40),
+    email varchar(255),
+    telefone INT, 
+    empresa varchar(255)
+ );
+ 
+ SELECT *
+ FROM  login l
+ JOIN  informacoes i
+ ON l.id_login = i.id_info;
+ 
+ INSERT INTO login
+ VALUES ('1','natanista','patinhofeio123');
+ 
+ SELECT * 
+ FROM informacoes;
+ 
+ INSERT INTO informacoes
+ VALUES ('1','Natan','Silva','natan@bandtec.com.br','119999','Google Inc.');
+ 
