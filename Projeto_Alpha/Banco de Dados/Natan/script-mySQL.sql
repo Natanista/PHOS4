ea0c2a845073ea8bf83b041b3e75aa9b363c1648
Natan Silva <gdaynatan@gmail.com>
2020-09-14 11:54:02 -0300

    Update script-mySQL.sql

--- Projeto_Alpha/Banco de Dados/Natan/script-mySQL.sql
+++ Projeto_Alpha/Banco de Dados/Natan/script-mySQL.sql
@@ -1,52 +1,36 @@
 CREATE DATABASE empresa;
 
 CREATE TABLE login(
-	id_login INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	id_login INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
 	login varchar(40) NOT NULL,
-    senha varchar(40) NOT NULL
+        senha varchar(40) NOT NULL
  );
  
- CREATE TABLE informacoes(
-	id_info INT  NOT NULL,
-    first_name varchar(40),
-    last_name varchar(40),
-    email varchar(255),
-    telefone INT, 
-    empresa varchar(255)
+ CREATE TABLE representante (
+ 	id_representante INT NOT NULL PRIMARY KEY, 
+	 nome_representante varchar (70),
+	 empresa_id INT
  );
  
- SELECT *
- FROM  login l
- JOIN  informacoes i
- ON l.id_login = i.id_info;
- 
- INSERT INTO login
- VALUES ('1','natanista','patinhofeio123');
- 
- SELECT * 
- FROM informacoes;
- 
- INSERT INTO informacoes
- VALUES ('1','Natan','Silva',
- 'natan@bandtec.com.br','119999','Google Inc.');
+ CREATE TABLE projeto (
+ id_empresa INT PRIMARY KEY NOT NULL, 
+ qtdLampadas INT,
+ potenciaIndividual INT,
+ hrsLigadas INT,
+ diasLigados INT
+ );
  
  CREATE TABLE empresa(
-	id_empresa INT NOT NULL,
-	endereço varchar(255),
+	 id_empresa INT NOT NULL,
+	 id_representante INT NOT NULL, 
+	 cnpj INT  NOT NULL,
+    nome_empresa varchar(40),
+    email varchar(255),
+    telefone INT ,
+    endereço varchar(255),
     bairro varchar(80),
     numero INT, 
     cep INT,
     cidade varchar(80),
     estado varchar(80)
  );
- 
- CREATE TABLE projeto (
- qtdLampadas int,
- potenciaIndividual int,
- hrsLigadas int,
- diasLigados
- 
- );
- 
- 
- 
