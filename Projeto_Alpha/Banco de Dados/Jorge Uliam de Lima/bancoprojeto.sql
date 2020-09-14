6fa73e9ad763cc1779853805097b63a5a8f1f19c
Jorge Uliam de Lima <70400260+JorgeUL@users.noreply.github.com>
2020-09-14 12:02:45 -0300

    Update bancoprojeto.sql

--- Projeto_Alpha/Banco de Dados/Jorge Uliam de Lima/bancoprojeto.sql
+++ Projeto_Alpha/Banco de Dados/Jorge Uliam de Lima/bancoprojeto.sql
@@ -3,7 +3,16 @@ use site_institucional;
 create table Usuario (
 id int primary key,
 nome varchar(60),
-empresa varchar(60),
 email varchar(60),
 telefone/celular varchar(11),
   );
+
+
+create table Empresa (
+  id int primary key,
+  foreign key (id) references Usuario (id),
+  empresa varchar(60),
+  cnpj char(14),
+  
+  
+  
