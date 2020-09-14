0a766360ece6aafc0f0bbc44016737a151959fff
akzv-oliveira <vinicius.soliveira@bandtec.com.br>
2020-09-14 12:05:58 -0300

    Atualização do Meu Banco

--- Projeto_Alpha/Banco de Dados/Vinícius Oliveira/Banco de Dados do Projeto.sql
+++ Projeto_Alpha/Banco de Dados/Vinícius Oliveira/Banco de Dados do Projeto.sql
@@ -5,64 +5,49 @@ create database DHProject;
 use DHProject ;
 -- Selecionado o Banco de Dados "DHProject"
 
-create table Usuario(
--- Cadastro do Usuário
-	idcadastro int primary key NOT NULL AUTO_INCREMENT,
-    nome varchar(20) NOT NULL,
-    email varchar(50),
-    senha varchar(20)) AUTO_INCREMENT = 1;
--- Criação da Tabela "Usuario"
--- Inserção dos campos (idcadastro, nome, email, senha)
-
 create table Empresa(
--- Banco de Dados das Empresas
-	CNPJ varchar(20) primary key NOT NULL,
-    nome varchar(20),
+-- Banco de Dados do Cadastro das Empresas / Usuário
+	idlogin int primary key NOT NULL AUTO_INCREMENT,
+	CNPJ varchar(20),
+    nome varchar(50),
+    senha varchar(50),
     telefone varchar(20),
-    email varchar (20));
+    email varchar (50),
+    CEP varchar(10));
 -- Criação da Tabela "Empresa"
--- Inserção dos campos (CNPJ, nome, telefone, email)
+-- Inserção dos campos (idlogin, CNPJ, nome, senha, telefone, email, CEP)
 
 create table Sensores(
 -- Banco de Dados do Sensor
-	idsensor int(10) primary key NOT NULL,
+	idsensor int primary key NOT NULL AUTO_INCREMENT,
     modelo varchar(20),
     dado_sensor varchar(20));
 -- Criação da Tabela "Sensores"
 -- Inserção dos campos (idsensor, modelo, dado_sensor)
-
-insert into usuario (nome, email, senha) values
-	('Vinícius Oliveira','vinysantosolivera@gmail.com','vini123');
--- Inserção de Dados do Usuario    
-
-    
-insert into empresa values
-	('18.781.203/0001-28','Enterprise','011 5291-3922','enterprise@gmail.com');
+  
+insert into empresa (CNPJ, nome, senha, telefone, email, CEP) values
+	('18.781.203/0001-28','Enterprise','Enter0123','011 5291-3922','enterprise@gmail.com','04937-000');
 -- Inserção de Dados da Empresa
     
-insert into sensores values
-	(1, 'ldr', '559 de luminosidade');
+insert into sensores (modelo, dado_sensor) values
+	('ldr', '559 de luminosidade');
 -- Inserção de Dados dos Sensores
     
-select * from usuario;
--- Consulta de Dados dos Usuários
-
 select * from empresa;
 -- Consulta de Dados das Empresas
-
 select * from sensores;
 -- Consulta de Dados dos Sensores
 	
 show tables;
 -- Consulta de Tabelas
 
-drop table usuario;
--- Deletar a tabela Usuario
 drop table empresa;
 -- Deletar a tabela Empresa
 drop table sensores;
 -- Deletar a tabela Sensores
 
+truncate table empresa;
+-- Limpar os dados da tabela Empresa
 truncate table sensores;
 -- Limpar os dados da tabela Sensores
     
