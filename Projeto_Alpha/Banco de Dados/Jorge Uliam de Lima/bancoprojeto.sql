50b6e863315ec88ebf6f6374f29f35e5e7288346
Jorge Uliam de Lima <70400260+JorgeUL@users.noreply.github.com>
2020-09-14 10:57:49 -0300

    meu banco de dados

---
+++ Projeto_Alpha/Banco de Dados/Jorge Uliam de Lima/bancoprojeto.sql
@@ -0,0 +1,8 @@
+create database site_institucional;
+use site_institucional;
+create table Usuario (
+id int primary key,
+nome varchar(60),
+empresa varchar(60),
+email varchar(60),
+telefone/celular varchar(11),
