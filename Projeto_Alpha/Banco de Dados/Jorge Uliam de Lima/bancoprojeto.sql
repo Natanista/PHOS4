334da8f1422c575ca93b834c1673a77b1002164f
Jorge Uliam de Lima <70400260+JorgeUL@users.noreply.github.com>
2020-09-15 23:28:21 -0300

    Update bancoprojeto.sql

--- Projeto_Alpha/Banco de Dados/Jorge Uliam de Lima/bancoprojeto.sql
+++ Projeto_Alpha/Banco de Dados/Jorge Uliam de Lima/bancoprojeto.sql
@@ -24,4 +24,14 @@ create table Empresa (
   id_lumiempresa int primary key,
   luminosidade_natural varchar(19),
   luminosidade_eletrica varchar(19),
+  horario char(8),
   );
+
+insert into Usuario values (1,'Jaime Barão Hoffman','contatojaimebarao@hotmail.com','baraohoff_m=an','1197656968'),
+                           (2,'Joyce Lothbrok','empresavivaz@gmail.com','loth0b-r_o..k','1122317477');
+                           
+insert into Empresa values (1,'Loja do Barao','008543000173','1125879945'),
+                           (2,'Vivaz contabilidade','657739000123','1122039548');
+
+insert into Luminosidade values (1,'45%','55%','17:10:09),
+                                (2,'78%','22%%'11:05:01);
