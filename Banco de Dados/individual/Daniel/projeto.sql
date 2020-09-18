36da6dc444992e856de872bc1c91cc2f1f2a04e7
Matheus Boaventura <70399449+lozanodev@users.noreply.github.com>
2020-09-18 10:27:34 -0300

    Adicionei inserts, select no db

--- Banco de Dados/individual/Daniel/projeto.sql
+++ Banco de Dados/individual/Daniel/projeto.sql
@@ -2,9 +2,11 @@ create database Projeto;
 
 use Projeto;
 
--- registro do user
+-- registro da empresa
+
 create table registrar(
-cpnj varchar(30) primary key,
+idRegistro int primary key,
+cpnj varchar(30),
 login varchar(50),
 nomeempresa varchar(30),
 telefone varchar(11),
@@ -15,13 +17,20 @@ senha varchar(50)
 -- backlog dos registros da API
 
 create table registros(
+idBacklog int primary key,
 densidadeiluminacao int,
 tempoiluminicao varchar(8),
 variacaoilumicao varchar(15),
 datailuminacao varchar(8)
 );
 
+-- Insersão de dados criados pelas empresas
 
+insert into registrar(100,12345678912,'empresalogin1','empresa1','63215847','empresa1@live.com','123456'),
+                     (101,21987654321,'empresalogin2','empresa2','51573214','empresa2@hotmail.com','password'),
+                     (102,54321487532,'empresalogin3','empresa3','21435461','empresa3@outlook.com','senha');
 
+-- mostrar todos os dados
 
+select * from registrar;
 
