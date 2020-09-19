0ee6e1bd89d6acddce7caf22c02d833bb9980f87
Vinícius Oliveira <vinicius.soliveira@bandtec.com.br>
2020-09-19 11:45:25 -0300

    Organização das linhas de comando do Banco

--- Banco de Dados/banco_de_dados.sql
+++ Banco de Dados/banco_de_dados.sql
@@ -34,50 +34,33 @@ CREATE TABLE empresa (
 SELECT *
 FROM login;
 
--- Natan
 INSERT INTO login 
 VALUES('1','natanista','@2e1oUdhu8QWQ80BWD'),
-	  ('2','LuizGsilva','ghghhghg'),
-	  ('3','ViniOliveira','vinivini321'),
-	  ('4', LuisFRocha','lflflflf');
+      ('2','LuizGsilva','ghghhghg'),
+      ('3','ViniOliveira','vinivini321'),
+      ('4', 'LuisFRocha','lflflflf'),
+      ('5','Carlos','carlos15');
 
 INSERT INTO empresa 
 VALUES('1','GOOGLE','987388111','google@gmail.com','1231312213'),
-	  ('2','iutube','923421421','arebaba@hotmail.com','4123432141'),
-	  ('3','Puma','977686316','puma@gmail.com','2839872346'),
-	  ('4','Git','99878787','git@hub.com', '5834232336');
+      ('2','iutube','923421421','arebaba@hotmail.com','4123432141'),
+      ('3','Puma','977686316','puma@gmail.com','2839872346'),
+      ('4','Git','99878787','git@hub.com', '5834232336'),
+      ('5','amazon','19933142','amazon@amazon.com','00021315129');
 
 INSERT INTO endereco
 VALUES('1','rua natenas','666','vila pumpulha','Sao Paulo','SP','09834203'),
-	  ('2','rua das oliveiras','16','vila do chaves','Mauá','SP','09361231'),
-	  ('3','rua das pumas','24','vila das feras','Guarulhos','SP','09382111'),
-	  ('4','rua das casas', '3112','jardim das lojas','Interlagos','SP','0531566');
+      ('2','rua das oliveiras','16','vila do chaves','Mauá','SP','09361231'),
+      ('3','rua das pumas','24','vila das feras','Guarulhos','SP','09382111'),
+      ('4','rua das casas', '3112','jardim das lojas','Interlagos','SP','0531566'),
+      ('5','rua caxias','17','carrao','Sao Paulo','Sao Paulo','0822558');
 	  
 INSERT INTO luminosidade 
 VALUES('1000lux','980lux','20lux'),
       ('1100lux','950lux','150lux'),
       ('1200lux','1000lux','200lux'),
-	('1300lux','1050lux','100lux');
-  
-  
-  
-  -- Matheus silva
-   
-   Inserts
-
-    INSERT INTO login
-VALUES ('01','Carlos','carlos15');
-
-INSERT INTO empresa
-VALUES ('02','amazon','19933142','amazon@amazon.com',
-'00021315129');
-
-INSERT INTO endereco
-VALUES ('03','rua caxias','17','carrao','Sao Paulo',
-'Sao Paulo','0822558');
-
-INSERT INTO luminosidade 
-VALUES ('500lux','300lux','200lux');
+      ('1300lux','1050lux','100lux'),
+      ('500lux','300lux','200lux');
     
     
     
